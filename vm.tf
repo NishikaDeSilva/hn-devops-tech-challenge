4ee4a7f8ab59b2681792d144e8c584ec483d9cc2
Nishika De Silva <nishika.desilva@insurwave.com>
2025-07-21 23:03:47 +0100

    Add jumpbox host

--- vm.tf
+++ vm.tf
@@ -9,3 +9,12 @@ module "private_vm" {
   rsa_public_key      = file(".ssh/hn_rsa.pub")
 }
 
+module "jumpbox_vm" {
+  source = "./modules/azure_vm"
+
+  resource_group_name  = azurerm_resource_group.az_resource_group.name
+  subnet_id            = azurerm_subnet.az_pub_subnet.id
+  vm_name              = "${module.naming.virtual_machine.name}-jumpbox"
+  enable_public_access = true
+  rsa_public_key       = file(".ssh/hn_rsa.pub")
+}
