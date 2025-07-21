0110169e66fa47f851149b7871c4ad5ad40d50a8
Nishika De Silva <desilvanishika73@gmail.com>
2025-07-21 23:02:53 +0100

    Update module to enable public access to vm

--- modules/azure_vm/outputs.tf
+++ modules/azure_vm/outputs.tf
@@ -1,4 +1,4 @@
-output "vm_private_ip_address" {
-  description = "Private IP address of the VM"
-  value       = azurerm_network_interface.az_nic.private_ip_address
+output "vm_ip_address" {
+  description = "IP address of the VM"
+  value       = var.enable_public_access ? azurerm_public_ip.az_pip.0.ip_address : azurerm_network_interface.az_nic.private_ip_address
 }
