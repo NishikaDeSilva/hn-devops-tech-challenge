fd23037e7cdbe203ffc35dcacd8b6af1a7787eac
Nishika De Silva <nishika.desilva@insurwave.com>
2025-07-23 00:28:10 +0100

    Add role assignment for vm ssh login

--- iam.tf
+++ iam.tf
@@ -11,13 +11,13 @@ resource "azuread_group" "vm_ssh_login" {
 
 resource "azuread_group_member" "vm_ssh_login_member" {
   for_each         = var.entra_ssh_enabled ? toset(data.azuread_users.users.0.object_ids) : toset([])
-  group_object_id  = azuread_group.vm_ssh_login.0.id
+  group_object_id  = azuread_group.vm_ssh_login.0.object_id
   member_object_id = each.value
 }
 
 resource "azurerm_role_assignment" "vm_user_login" {
   count                = var.entra_ssh_enabled ? 1 : 0
   principal_id         = azuread_group.vm_ssh_login.0.object_id
-  role_definition_name = "Virtual Machine Local User Login"
+  role_definition_name = "Virtual Machine User Login"
   scope                = azurerm_resource_group.az_resource_group.id
 }
