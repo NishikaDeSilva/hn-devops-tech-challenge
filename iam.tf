4d0530c53f7a0be8c6a99e267be6b3ecaf4dd8e0
Nishika De Silva <desilvanishika73@gmail.com>
2025-07-22 23:49:14 +0100

    Add users for vm login

---
+++ iam.tf
@@ -0,0 +1,23 @@
+data "azuread_users" "users" {
+  count      = var.entra_ssh_enabled ? 1 : 0
+  return_all = true # returning all for this exercise
+}
+
+resource "azuread_group" "vm_ssh_login" {
+  count            = var.entra_ssh_enabled ? 1 : 0
+  display_name     = "vm_ssh_login"
+  security_enabled = true
+}
+
+resource "azuread_group_member" "vm_ssh_login_member" {
+  for_each         = var.entra_ssh_enabled ? toset(data.azuread_users.users.0.object_ids) : toset([])
+  group_object_id  = azuread_group.vm_ssh_login.0.id
+  member_object_id = each.value
+}
+
+resource "azurerm_role_assignment" "vm_user_login" {
+  count                = var.entra_ssh_enabled ? 1 : 0
+  principal_id         = azuread_group.vm_ssh_login.0.object_id
+  role_definition_name = "Virtual Machine Local User Login"
+  scope                = azurerm_resource_group.az_resource_group.id
+}
