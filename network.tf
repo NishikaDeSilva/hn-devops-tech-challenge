a80cce90eb307c64954a257e51fe16395298842f
Nishika De Silva <nishika.desilva@insurwave.com>
2025-07-23 23:31:18 +0100

    Add tags and update description

--- network.tf
+++ network.tf
@@ -3,6 +3,8 @@ resource "azurerm_virtual_network" "az_vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.az_resource_group.name
+
+  tags = local.tags
 }
 
 resource "azurerm_subnet" "az_pvt_subnet" {
