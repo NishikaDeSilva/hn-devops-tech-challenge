75a4439b2f1119cf96470f00efc06295eaa36f5b
Nishika De Silva <desilvanishika73@gmail.com>
2025-07-23 21:22:00 +0100

    Update README

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "vm_1_private_ip_address" {
 output "jumpbox_public_ip" {
   value = module.jumpbox_vm.vm_ip_address
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.az_resource_group.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.az_storage.name
+}
